4a0870b9d2df658b8c838a94d1ac2245781c23f2
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-09-01 17:32:51 +0200

    Add program.js

---
+++ program.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === '/exit') {
+            process.stdout.write('Quiting app!\n');
+            process.exit();
+        } else {
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+})
