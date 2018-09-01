cd1b2f93f9a40bbe06e238814b372061a6176daa
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-09-01 21:00:09 +0200

    Change function into module getOSinfo

--- app/program.js
+++ app/program.js
@@ -1,4 +1,5 @@
 var os = require('os'); 
+var OSinfo = require('../modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
 
@@ -16,30 +17,11 @@ process.stdin.on('readable', function() {
                 process.exit();
                 break;
             case '/getOSinfo':
-                getOSinfo();
+                OSinfo.print();
                 break;
             default:
                 process.stderr.write('Wrong instruction!\n');
         }
     }
 })
-
-function getOSinfo() {
-    var type = os.type();
-    if (type === 'Darwin') {
-        type = 'OSX';
-    } else if (type === 'Windows_NT') {
-        type = 'Windows';
-    }
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
-    var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime:~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
-}
 
