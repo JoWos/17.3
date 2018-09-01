57754300f8ba342b4739c21a66ab86b311063323
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-09-01 20:08:57 +0200

    Add function getOSinfo

--- program.js
+++ program.js
@@ -1,3 +1,5 @@
+var os = require('os'); 
+
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
@@ -13,10 +15,31 @@ process.stdin.on('readable', function() {
                 process.stdout.write('Quitting app!\n');
                 process.exit();
                 break;
+            case '/getOSinfo':
+                getOSinfo();
+                break;
             default:
                 process.stderr.write('Wrong instruction!\n');
         }
-        
     }
 })
+
+function getOSinfo() {
+    var type = os.type();
+    if (type === 'Darwin') {
+        type = 'OSX';
+    } else if (type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime:~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
 
