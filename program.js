4ef5a39d1ee7572fbfd7868836e2996af02008f7
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-09-01 18:40:19 +0200

    Update js

--- program.js
+++ program.js
@@ -2,13 +2,21 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
+    
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quiting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!\n');
+        switch (instruction) {
+            case 'process.env':
+                console.log(process.env);
+                break;
+            case '/exit':
+                process.stdout.write('Quiting app!\n');
+                process.exit();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
         }
+        
     }
 })
+
