f88f73cb9236532f4ba89143397d0562c0f89d54
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-09-04 08:07:38 +0200

    Update js

--- modules/timeinfo.js
+++ modules/timeinfo.js
@@ -4,7 +4,7 @@ function timeInfo() {
     var uptime = os.uptime();
     var hour = (uptime / 3600).toFixed(0);
     var min = ((uptime % 3600) / 60).toFixed(0);
-    var sek = ((uptime % 3600) % 60).toFixed(0);
+    var sek = (uptime % 60).toFixed(0);
     console.log('Uptime:~', hour, 'h', min, 'min', sek, 'sec');
 }
 
